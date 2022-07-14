4c9b0cc1b67b5de02b93dc6d146bded7029ccd76
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-14 08:45:29 -0300

    nice

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ var x1 = 0;
 var y1 = 0;
 var x2 = 0;
 var y2 = 0;
+
+function update() {
+    document.getElementById("score").style.top = y1 + "px"
+    document.getElementById("snake").style.top = y2 + "px"
+    document.getElementById("score").style.left = x1 + "px"
+    document.getElementById("snake").style.left = x2 + "px"
+}
