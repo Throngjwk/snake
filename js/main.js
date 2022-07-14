ce45c2b7fb8f264f0a1feb02d659b76cbc0c8f01
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-14 08:51:55 -0300

    cool

--- js/main.js
+++ js/main.js
@@ -6,6 +6,22 @@ var y1 = 0;
 var x2 = 0;
 var y2 = 0;
 
+document.getElementById("left").onclick = () => {
+    x2 += 5
+}
+
+document.getElementById("right").onclick = () => {
+    x2 -= 5
+}
+
+document.getElementById("up").onclick = () => {
+    y2 += 5
+}
+
+document.getElementById("down").onclick = () => {
+    y2 -= 5
+}
+
 function update() {
     document.getElementById("score").style.top = y1 + "px"
     document.getElementById("snake").style.top = y2 + "px"
